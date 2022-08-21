f8af4e5fc0e80619764d693729c5dcaac8f972a3
xzxin <xiazhenxin123@126.com>
2022-08-21 16:24:56 +0800

    增加首页，数据查询以及联动

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,74 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/index/index.js
+const utils = require('../../utils/util');
 Page({
+
+    /**
+     * 页面的初始数据
+     */
     data: {
-        roles: [
-          {value: 'clx', path: "../../images/clx.png", reward: "", label: "陈立祥"},
-          {value: 'jql', path: "../../images/jql.png", reward: "", label: "金秋龙"},
-          {value: 'xzx', path: "../../images/xzx.png", reward: "", label: "夏振新"},
-          {value: 'tyt', path: "../../images/tyt.png", reward: "", label: "谭言仝"},
-          {value: 'stb', path: "../../images/stb.png", reward: "", label: "施廷波"},
-          {value: 'hp', path: "../../images/hp.png", reward: "", label: "黄鹏"},
-          {value: 'glh', path: "../../images/glh.png", reward: "", label: "郭立恒"},
-          {value: 'sy', path: "../../images/sy.png", reward: "", label: "石岩"}
-        ]
-      },
-      press() {
-        wx.setStorageSync("key","value");
-        console.log(wx.getStorageSync('key'))
-      },
-      tapCheck(e) {
-        const roles = this.data.roles
-      }
+
+    },
+
+    navigateToStartGame() {
+        wx.navigateTo({
+          url: '/pages/start_game/start_game',
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+        var user_info_list = utils.queryUserRank()
+        console.log(user_info_list)
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
 })
--- utils/util.js
+++ utils/util.js
@@ -9,11 +9,113 @@ const formatTime = date => {
   return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
 }
 
+const queryUserInfoList = () => {
+    var user_info_list = wx.getStorageSync('user_info_list')
+    if (user_info_list === "") {
+        user_info_list = user_default_info_list()
+        wx.setStorageSync('user_info_list', user_info_list)
+    }
+    return user_info_list;
+}
+
+const queryUserRank = () => {
+    var user_info_list = queryUserInfoList()
+    for (let i=0; i<user_info_list.length - 1; i++) {
+        for (let j= 0; j < user_info_list.length - 1 - i; j++) {
+            if (user_info_list[j]["winRate"] < user_info_list[j+1]["winRate"]) {
+                let temp = user_info_list[j]
+                user_info_list[j] = user_info_list[j+1]
+                user_info_list[j+1] = temp
+            }
+        }
+    }
+    return user_info_list
+}
+
+const user_default_info_list = () => {
+    var default_info_list = [
+        {
+            "name": "clx",
+            "total": 0,
+            "win": 0,
+            "winRate": 80,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        },
+        {
+            "name": "jql",
+            "total": 0,
+            "win": 0,
+            "winRate": 70,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        },
+        {
+            "name": "xzx",
+            "total": 0,
+            "win": 0,
+            "winRate": 100,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        },
+        {
+            "name": "tyt",
+            "total": 0,
+            "win": 92,
+            "winRate": 0,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        },
+        {
+            "name": "stb",
+            "total": 42,
+            "win": 0,
+            "winRate": 0,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        },
+        {
+            "name": "hp",
+            "total": 79,
+            "win": 0,
+            "winRate": 0,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        },
+        {
+            "name": "glh",
+            "total": 0,
+            "win": 0,
+            "winRate": 0,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        },
+        {
+            "name": "sy",
+            "total": 0,
+            "win": 0,
+            "winRate": 0,
+            "blastTotal": 0,
+            "blastWin": 0,
+            "blastWinRate": 0
+        }
+    ]
+    return default_info_list;
+  }
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : `0${n}`
 }
 
 module.exports = {
-  formatTime
+  formatTime,
+  queryUserRank
 }
